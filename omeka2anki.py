0d3bff15efe571b00cf436ae60fe6288850b2a01
Brian Dunnette <dunn0172@umn.edu>
2013-08-13 16:07:50 -0500

    trying to save files in more OS-agnostic way

--- omeka2anki.py
+++ omeka2anki.py
@@ -7,18 +7,23 @@ import json
 import sys
 import pprint
 from urllib import urlretrieve
+import os.path
+
 import omeka_api_config
+
 import anki, anki.exporting
 
 api_endpoint = omeka_api_config.API_ENDPOINT
 
+root_path = os.path.join(os.path.expanduser('~'), 'omeka2anki-temp')
+
 omeka_collections = requests.get(api_endpoint + "collections").json()
-for omeka_collection in omeka_collections:
+for omeka_collection in omeka_collections[1:]:
     collection_name = omeka_collection['element_texts'][0]['text'].lower().replace(' ','_')
     omeka_items = requests.get(omeka_collection['items']['url']).json()
     print "Found collection %s with %s items" % (collection_name, len(omeka_items))#pprint.pprint(omeka_collection)
     if len(omeka_items) >= 1:
-        anki_collection = anki.storage.Collection("%s.anki2" % collection_name)
+        anki_collection = anki.storage.Collection(os.path.join(root_path, "%s.anki2" % collection_name))
         for item in omeka_items:
             #print item
             item_files = requests.get(item['files']['url']).json()
@@ -28,15 +33,15 @@ for omeka_collection in omeka_collections:
                 if file['element_texts'] and 'image' in file['mime_type']:
                     anki_note = anki_collection.newNote()
                     print "Downloading file %s" % file['filename']
-                    file_image = urlretrieve(file['file_urls']['thumbnail'], file['filename'])
+                    file_image = urlretrieve(file['file_urls']['original'], os.path.join(root_path, file['filename']))
                     card_back = file['element_texts'][0]['text']
                     anki_note.fields = ["<img src='%s'>" % file['filename'], card_back]
                     anki_collection.addNote(anki_note)
                     # pprint.pprint(anki_note.__dict__)
-                    anki_collection.media.addFile(file['filename'])
+                    anki_collection.media.addFile(os.path.join(root_path, file['filename']))
 
         print "Saving collection %s" % collection_name 
         anki_collection.save()
         anki_exporter = anki.exporting.AnkiPackageExporter(anki_collection)
         print "Exporting to %s.apkg" % collection_name
-        anki_exporter.exportInto("C:\\temp\\%s.apkg" % collection_name)
+        anki_exporter.exportInto(os.path.join(root_path, "%s.apkg" % collection_name))
